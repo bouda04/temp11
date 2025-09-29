4b1427525f453b5f1b32185e175869b1a8bd3d4a
Daniel Bouenos <bouda04@gmail.com>
2025-09-29 15:11:16 +0300

    new layout

--- app/src/main/java/com/example/temp11/MainActivity.java
+++ app/src/main/java/com/example/temp11/MainActivity.java
@@ -16,15 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //comment
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout2b);
         //comment
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        TextView tv = findViewById(R.id.tvHello);
-        tv.setText("Hello!");
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+
 
     }
 }
